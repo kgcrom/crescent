abfd33740e6d088825a44e8108819e6eb26f21ae
kgcrom <kgcrom@hotmail.com>
2021-02-27 20:05:42 +0900

    feature: spring boot 2.4로 구동되도록 수정
    - jsp가 아닌 thymeleaf 템플릿 엔진으로 작업하도록 세팅

---
+++ crescent_core_web/src/main/java/com/tistory/devyongsik/crescent/CrescentApplication.java
@@ -0,0 +1,13 @@
+package com.tistory.devyongsik.crescent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+public class CrescentApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CrescentApplication.class, args);
+  }
+}
